eecd396cbbe1c6a49ad912a0720e9795fa18f6d9
choe6243@wrdsb.ca <choe6243@wrdsb.ca>
2022-01-27 15:46:18 +0000

    colour adjustment

---
+++ UBMP4-Intro-2-Variables.X/Neonpixel.c
@@ -0,0 +1,197 @@
+/*==============================================================================
+ Project: NeoPixel-Colour-Adjust
+ Date:    January 26, 2022
+ 
+ This program demonstrates basic NeoPixel data transmission & colour adjustment.
+==============================================================================*/
+
+#include    "xc.h"              // Microchip XC8 compiler include file
+#include    "stdint.h"          // Include integer definitions
+#include    "stdbool.h"         // Include Boolean (true/false) definitions
+
+#include    "UBMP4.h"           // Include UBMP4 constants and functions
+
+// TODO Set linker ROM ranges to 'default,-0-7FF' under "Memory model" pull-down.
+// TODO Set linker code offset to '800' under "Additional options" pull-down.
+
+// Program constant definitions
+#define LEDs 10
+
+// Program variable definitions
+unsigned char red;              // Colour or colour-adjusted output values
+unsigned char green;
+unsigned char blue;
+
+unsigned char red_index = 10;   // Raw colour index values
+unsigned char green_index = 10;
+unsigned char blue_index = 10;
+
+bool red_up = true;             // Colour adjustment direction variables
+bool green_up = true;
+bool blue_up = true;
+
+// Gamma 1.8 colour output look-up table
+const char gamma[256] = {
+0, 0, 0, 0, 0, 0, 0, 0,
+1, 1, 1, 1, 1, 1, 1, 2,
+2, 2, 2, 2, 3, 3, 3, 3,
+4, 4, 4, 4, 5, 5, 5, 6,
+6, 6, 7, 7, 8, 8, 8, 9,
+9, 10, 10, 10, 11, 11, 12, 12,
+13, 13, 14, 14, 15, 15, 16, 16,
+17, 17, 18, 18, 19, 19, 20, 21,
+21, 22, 22, 23, 24, 24, 25, 26,
+26, 27, 28, 28, 29, 30, 30, 31,
+32, 32, 33, 34, 35, 35, 36, 37,
+38, 38, 39, 40, 41, 41, 42, 43,
+44, 45, 46, 46, 47, 48, 49, 50,
+51, 52, 53, 53, 54, 55, 56, 57,
+58, 59, 60, 61, 62, 63, 64, 65,
+66, 67, 68, 69, 70, 71, 72, 73,
+74, 75, 76, 77, 78, 79, 80, 81,
+82, 83, 84, 86, 87, 88, 89, 90,
+91, 92, 93, 95, 96, 97, 98, 99,
+100, 102, 103, 104, 105, 107, 108, 109,
+110, 111, 113, 114, 115, 116, 118, 119,
+120, 122, 123, 124, 126, 127, 128, 129,
+131, 132, 134, 135, 136, 138, 139, 140,
+142, 143, 145, 146, 147, 149, 150, 152,
+153, 154, 156, 157, 159, 160, 162, 163,
+165, 166, 168, 169, 171, 172, 174, 175,
+177, 178, 180, 181, 183, 184, 186, 188,
+189, 191, 192, 194, 195, 197, 199, 200,
+202, 204, 205, 207, 208, 210, 212, 213,
+215, 217, 218, 220, 222, 224, 225, 227,
+229, 230, 232, 234, 236, 237, 239, 241,
+243, 244, 246, 248, 250, 251, 253, 255};
+
+
+// Shift 8-bits of NeoPixel data
+void neopixel_shift(unsigned char col)
+{
+    for(unsigned char bits = 8; bits != 0; bits --)
+    {
+        H1OUT = 1;
+        if((col & 0b10000000) == 0)
+        {
+            H1OUT = 0;
+        }
+        NOP();
+        H1OUT = 0;
+        col = col << 1;
+    }
+}
+
+// Fill NeoPixel strip with colour from the red, green, and blue variables
+void neopixel_fill(unsigned char leds)
+{
+    for(leds; leds != 0; leds--)
+    {
+        neopixel_shift(green);
+        neopixel_shift(red);
+        neopixel_shift(blue);
+    }
+}
+
+
+int main(void)
+{
+    OSC_config();               // Configure internal oscillator for 48 MHz
+    UBMP4_config();             // Configure on-board UBMP4 I/O devices
+    
+    while(1)
+    {
+        if(SW3 == 0)            // Adjust red index value
+        {
+            if(red_up == true)
+            {
+                if(red_index < 255)
+                {
+                    red_index++;
+                }
+                else
+                {
+                    red_up = false;
+                }
+            }
+            else
+            {
+                if(red_index > 0)
+                {
+                    red_index--;
+                }
+                else
+                {
+                    red_up = true;
+                }
+            }
+        }
+        
+        if(SW4 == 0)            // Adjust green index value
+        {
+            if(green_up == true)
+            {
+                if(green_index < 255)
+                {
+                    green_index++;
+                }
+                else
+                {
+                    green_up = false;
+                }
+            }
+            else
+            {
+                if(green_index > 0)
+                {
+                    green_index--;
+                }
+                else
+                {
+                    green_up = true;
+                }
+            }
+        }
+        
+        if(SW5 == 0)            // Adjust blue index value
+        {
+            if(blue_up == true)
+            {
+                if(blue_index < 255)
+                {
+                    blue_index++;
+                }
+                else
+                {
+                    blue_up = false;
+                }
+            }
+            else
+            {
+                if(blue_index > 0)
+                {
+                    blue_index--;
+                }
+                else
+                {
+                    blue_up = true;
+                }
+            }
+        }
+        
+        // Gamma adjust output colours
+        red = gamma[red_index];
+        green = gamma[green_index];
+        blue = gamma[blue_index];
+        
+        // Fill the strip with adjusted colours
+        neopixel_fill(LEDs);
+        __delay_ms(20);
+
+        // Activate bootloader if SW1 is pressed.
+        if(SW1 == 0)
+        {
+            RESET();
+        }
+    }
+}
--- UBMP4-Intro-2-Variables.X/nbproject/Makefile-default.mk
+++ UBMP4-Intro-2-Variables.X/nbproject/Makefile-default.mk
@@ -30,12 +30,12 @@ ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 IMAGE_TYPE=debug
 OUTPUT_SUFFIX=elf
 DEBUGGABLE_SUFFIX=elf
-FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+FINAL_IMAGE=${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 else
 IMAGE_TYPE=production
 OUTPUT_SUFFIX=hex
 DEBUGGABLE_SUFFIX=elf
-FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+FINAL_IMAGE=${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 endif
 
 ifeq ($(COMPARE_BUILD), true)
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=UBMP4.c Intro-2-Variables.c
+SOURCEFILES_QUOTED_IF_SPACED=UBMP4.c Neonpixel.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/UBMP4.p1 ${OBJECTDIR}/Intro-2-Variables.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/UBMP4.p1.d ${OBJECTDIR}/Intro-2-Variables.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/UBMP4.p1 ${OBJECTDIR}/Neonpixel.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/UBMP4.p1.d ${OBJECTDIR}/Neonpixel.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/UBMP4.p1 ${OBJECTDIR}/Intro-2-Variables.p1
+OBJECTFILES=${OBJECTDIR}/UBMP4.p1 ${OBJECTDIR}/Neonpixel.p1
 
 # Source Files
-SOURCEFILES=UBMP4.c Intro-2-Variables.c
+SOURCEFILES=UBMP4.c Neonpixel.c
 
 
 
@@ -88,7 +88,7 @@ FIXDEPS=fixDeps
 ifneq ($(INFORMATION_MESSAGE), )
 	@echo $(INFORMATION_MESSAGE)
 endif
-	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
 MP_PROCESSOR_OPTION=16F1459
 # ------------------------------------------------------------------------------------
@@ -98,34 +98,34 @@ ${OBJECTDIR}/UBMP4.p1: UBMP4.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UBMP4.p1.d 
 	@${RM} ${OBJECTDIR}/UBMP4.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/UBMP4.p1 UBMP4.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/UBMP4.p1 UBMP4.c 
 	@-${MV} ${OBJECTDIR}/UBMP4.d ${OBJECTDIR}/UBMP4.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/UBMP4.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/Intro-2-Variables.p1: Intro-2-Variables.c  nbproject/Makefile-${CND_CONF}.mk 
+${OBJECTDIR}/Neonpixel.p1: Neonpixel.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/Intro-2-Variables.p1.d 
-	@${RM} ${OBJECTDIR}/Intro-2-Variables.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Intro-2-Variables.p1 Intro-2-Variables.c 
-	@-${MV} ${OBJECTDIR}/Intro-2-Variables.d ${OBJECTDIR}/Intro-2-Variables.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/Intro-2-Variables.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/Neonpixel.p1.d 
+	@${RM} ${OBJECTDIR}/Neonpixel.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c  -D__DEBUG=1  -mdebugger=none   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Neonpixel.p1 Neonpixel.c 
+	@-${MV} ${OBJECTDIR}/Neonpixel.d ${OBJECTDIR}/Neonpixel.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Neonpixel.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/UBMP4.p1: UBMP4.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/UBMP4.p1.d 
 	@${RM} ${OBJECTDIR}/UBMP4.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/UBMP4.p1 UBMP4.c 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/UBMP4.p1 UBMP4.c 
 	@-${MV} ${OBJECTDIR}/UBMP4.d ${OBJECTDIR}/UBMP4.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/UBMP4.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-${OBJECTDIR}/Intro-2-Variables.p1: Intro-2-Variables.c  nbproject/Makefile-${CND_CONF}.mk 
+${OBJECTDIR}/Neonpixel.p1: Neonpixel.c  nbproject/Makefile-${CND_CONF}.mk 
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/Intro-2-Variables.p1.d 
-	@${RM} ${OBJECTDIR}/Intro-2-Variables.p1 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Intro-2-Variables.p1 Intro-2-Variables.c 
-	@-${MV} ${OBJECTDIR}/Intro-2-Variables.d ${OBJECTDIR}/Intro-2-Variables.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/Intro-2-Variables.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	@${RM} ${OBJECTDIR}/Neonpixel.p1.d 
+	@${RM} ${OBJECTDIR}/Neonpixel.p1 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -c   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -DXPRJ_default=$(CND_CONF)  -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits $(COMPARISON_BUILD)  -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     -o ${OBJECTDIR}/Neonpixel.p1 Neonpixel.c 
+	@-${MV} ${OBJECTDIR}/Neonpixel.d ${OBJECTDIR}/Neonpixel.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/Neonpixel.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
@@ -144,15 +144,15 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: link
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
-	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -std=c99 -gdwarf-3 -mstack=compiled:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -o dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
-	@${RM} dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.hex 
+${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.map  -D__DEBUG=1  -mdebugger=none  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto        $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	@${RM} ${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.hex 
 	
 else
-dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
-	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.map  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -o dist/${CND_CONF}/${IMAGE_TYPE}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} ${DISTDIR} 
+	${MP_CC} $(MP_EXTRA_LD_PRE) -mcpu=$(MP_PROCESSOR_OPTION) -Wl,-Map=${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.map  -DXPRJ_default=$(CND_CONF)  -Wl,--defsym=__MPLAB_BUILD=1   -mdfp="${DFP_DIR}/xc8"  -fno-short-double -fno-short-float -mrom=default,-0-07FF -O0 -fasmfile -maddrqual=ignore -xassembler-with-cpp -mwarn=-3 -mext=cci -Wa,-a -msummary=-psect,-class,+mem,-hex,-file -mcodeoffset=800  -ginhx32 -Wl,--data-init -mno-keep-startup -mno-osccal -mno-resetbits -mno-save-resetbits -mno-download -mno-stackcall -mdefault-config-bits -std=c99 -gdwarf-3 -mstack=compiled:auto:auto     $(COMPARISON_BUILD) -Wl,--memorysummary,${DISTDIR}/memoryfile.xml -o ${DISTDIR}/UBMP4-Intro-2-Variables.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	
 endif
 
@@ -166,8 +166,8 @@ endif
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
-	${RM} -r build/default
-	${RM} -r dist/default
+	${RM} -r ${OBJECTDIR}
+	${RM} -r ${DISTDIR}
 
 # Enable dependency checking
 .dep.inc: .depcheck-impl
